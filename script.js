c621f5b0ffef5b68c0252ffe6e38d7208415376b
Yeltsin Anwar Reyes Blandón <yeltsin.reyes95@gmail.com>
2023-08-21 00:20:24 -0600

    Probando funciones de Operadores

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const pantalla = document.getElementById("pantalla");
 });
 
+const Operadores = {
+  Multiplicar: 1,
+  Dividir: 2,
+  Sumar: 3,
+  Restar: 4,
+  Porcentaje: 5,
+};
+let Operando = 0;
+let Operador = 0;
+
 function teclearNumero(numero) {
   if (pantalla.innerHTML.length < 10) {
     if (pantalla.innerHTML === "0") {
@@ -13,9 +23,38 @@ function teclearNumero(numero) {
 }
 
 function borrarTodo() {
-    pantalla.innerHTML = "0";
+  pantalla.innerHTML = "0";
 }
 
 function borrar() {
-    pantalla.innerHTML = pantalla.innerHTML.slice(0, -1);
+  pantalla.innerHTML = pantalla.innerHTML.slice(0, -1);
+}
+
+function masMenos() {
+  if (pantalla.innerHTML !== "0" && pantalla.innerHTML[0] !== "-") {
+    pantalla.innerHTML = "-" + pantalla.innerHTML;
+  } else {
+    pantalla.innerHTML = pantalla.innerHTML.slice(1);
+  }
+}
+
+function teclearOperacion(op) {
+  switch (op) {
+    case Operadores.Multiplicar:
+      Operador = Operadores.Multiplicar;
+      break;
+    case Operadores.Dividir:
+      Operador = Operadores.Dividir;
+      break;
+    case Operadores.Sumar:
+      Operador = Operadores.Sumar;
+      break;
+    case Operadores.Restar:
+      Operador = Operadores.Restar;
+      break;
+    case Operadores.Porcentaje:
+      Operador = Operadores.Porcentaje;
+      break;
+  }
+  Operando = parseFloat(pantalla.innerHTML);
 }
